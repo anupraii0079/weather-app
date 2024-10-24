69f294f457e5054fac1f1b3aec1b8dd0547fb1af
Anup <anuprai8874@gmail.com>
2024-10-24 12:48:18 +0530

    first commit

---
+++ src/components/search/index.jsx
@@ -0,0 +1,18 @@
+import "/src/App.css"
+export default function Search({ search, setSearch, handleSearch }) {
+  return (
+    <div className="search-engine">
+      <input
+        type="text"
+        // className="city-search"
+        placeholder="Enter City Name"
+        name="search"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <button  onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
+}
